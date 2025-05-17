49a5b2439a3fc3bed5843931ba9142a5fc6c002e
luantreia <luantreia@gmail.com>
2025-05-17 19:21:15 -0300

    Actualización automática

--- src/components/models/ModalEquipo.js
+++ src/components/models/ModalEquipo.js
@@ -87,7 +87,7 @@ const styles = {
   overlay: {
     position: 'fixed',
     top: 0, left: 0, right: 0, bottom: 0,
-    height: '100dvh',
+    height: '90dvh',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
