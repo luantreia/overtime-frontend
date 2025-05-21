b630191110ae1333e79471f50c6c4c607371afd7
luantreia <luantreia@gmail.com>
2025-05-21 02:11:42 -0300

    Actualización automática

--- src/hooks/useUserRole.js
+++ src/hooks/useUserRole.js
@@ -1,9 +1,9 @@
 // src/hooks/useUserRole.js
 import { useContext } from 'react';
-import { useAuth } from '../context/Authcontext';
+import { AuthContext } from '../context/Authcontext';
 
 const useUserRole = () => {
-  const { user, rol } = useContext(useAuth);
+  const { user, rol } = useContext(AuthContext);
   const uid = user?.uid || null;
   return { rol, uid };
 };
